681804821bc939e44dc99acc2d754d759f9668fe
angelcriber99 <angelcriber99@gmail.com>
2022-07-12 20:58:15 +0200

    añadir valores js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Número Correcto';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
